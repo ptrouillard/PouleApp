4d9471ed2d23a51bb6b8ad5f17efdf19da8cecb2
ptrouillard <ptrouillard@gmail.com>
2020-06-09 12:24:08 +0200

    Initial commit

---
+++ src/main/java/com/pedro/raspberry/poule/PouleUiApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.raspberry.poule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PouleUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PouleUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/raspberry/poule/PouleUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.raspberry.poule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouleUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
